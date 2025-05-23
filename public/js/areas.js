335f7cd88cfd673e8c03ed4e8bdc02052d389fed
walterossanti35@gmail.com <walterossanti35@gmail.com>
2025-05-23 13:34:15 -0500

    corrccion con el menu de areas 23-05-25

--- public/js/areas.js
+++ public/js/areas.js
@@ -13,4 +13,13 @@ document.querySelectorAll('.confuguracion-area').forEach((boton) => {
       if (m !== menu) m.classList.remove('activo');
     });
   });
+  document.addEventListener('click', (e) => {
+  // Verifica si el clic fue fuera de cualquier menú o botón de configuración
+  if (!e.target.closest('.area-menu') && !e.target.closest('.confuguracion-area')) {
+    document.querySelectorAll('.area-menu.activo').forEach((menu) => {
+      menu.classList.remove('activo');
+    });
+  }
 });
+});
+
